1911fa51fb96012d304b465051e5c31aa47e69f5
Sidakov21 <amirsidacov09@gmail.com>
2025-12-04 14:20:22 +0300

    Реализация фильтрации и поиска

--- УП 2 по ТРПО Сервис частных объявлений/MainWindow.xaml.cs
+++ УП 2 по ТРПО Сервис частных объявлений/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.Entity;
 
 namespace УП_2_по_ТРПО_Сервис_частных_объявлений
 {
@@ -28,6 +29,7 @@ namespace УП_2_по_ТРПО_Сервис_частных_объявлений
         {
             InitializeComponent();
             LoadAds();
+            LoadFilters();
         }
 
         public void LoadAds()
@@ -39,9 +41,80 @@ namespace УП_2_по_ТРПО_Сервис_частных_объявлений
             }
         }
 
+        private void LoadFilters()
+        {
+            // Город
+            CityFilter.ItemsSource = _context.Cities.ToList();
+            CityFilter.DisplayMemberPath = "city_name";
+            // Категория
+            CategoryFilter.ItemsSource = _context.Categories.ToList();
+            CategoryFilter.DisplayMemberPath = "category_name";
+            // Тип
+            TypeFilter.ItemsSource = _context.Ad_Types.ToList();
+            TypeFilter.DisplayMemberPath = "type_name";
+            // Статус
+            StatusFilter.ItemsSource = _context.Ad_Statuses.ToList();
+            StatusFilter.DisplayMemberPath = "status_name";
+
+            // Добавляем возможность сброса (опционально)
+            CityFilter.SelectedIndex = -1;
+            CategoryFilter.SelectedIndex = -1;
+            TypeFilter.SelectedIndex = -1;
+            StatusFilter.SelectedIndex = -1;
+        }
+
+        public void ApplyFiltersAndLoadAds()
+        {
+            IQueryable<Ads> query = _context.Ads
+                .Include(a => a.Cities)
+                .Include(a => a.Categories)
+                .Include(a => a.Ad_Types)
+                .Include(a => a.Ad_Statuses);
+
+            string search = SearchBox.Text.ToLower().Trim();
+
+            if (StatusFilter.SelectedIndex == -1)
+            {
+                query = query.Where(a => a.Ad_Statuses.status_name == "Активно");
+            }
+
+
+            if (!string.IsNullOrWhiteSpace(search))
+            {
+                query = query.Where(a =>
+                    a.ad_title.ToLower().Contains(search) ||
+                    a.ad_description.ToLower().Contains(search)
+                );
+            }
+
+            if (CityFilter.SelectedItem is Cities selectedCity)
+            {
+                query = query.Where(a => a.city_id == selectedCity.city_id);
+            }
+
+
+            if (CategoryFilter.SelectedItem is Categories selectedCategory)
+            {
+                query = query.Where(a => a.category_id == selectedCategory.category_id);
+            }
+
+
+            if (TypeFilter.SelectedItem is Ad_Types selectedType)
+            {
+                query = query.Where(a => a.ad_type_id == selectedType.type_id);
+            }
+
+            if (StatusFilter.SelectedItem is Ad_Statuses selectedStatus)
+            {
+                query = query.Where(a => a.ad_status_id == selectedStatus.status_id);
+            }
+
+            AdsList.ItemsSource = query.ToList();
+        }
+
         private void Search_Click(object sender, RoutedEventArgs e)
         {
-
+            ApplyFiltersAndLoadAds();
         }
 
     }
